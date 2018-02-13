fe30fcfa653247c6fbdf5c988bbb5a1b083df0fb
Unknown <carlosdavid_229@hotmail.com>
2018-02-13 09:30:09 -0500

    Primera Clase
    Ejercicio Nº 1 en java con suma, resta, multiplicacion y division.

---
+++ Main.java
@@ -0,0 +1,21 @@
+public class Main{
+    public static void main(String[] arg){
+        System.out.println("");
+        System.out.println("Clase de Programacion II");
+        System.out.println("");
+        System.out.println("\n\n\t Ejercicio Nº 1");
+        System.out.println("");
+        System.out.println("");
+
+        int var1 = 50;
+        int var2 = 20;
+        float division = (var1/var2);
+
+        System.out.println("La suma de los numeros "+var1+"+"+var2+" es= "+(var1+var2));
+        System.out.println("La resta de los numeros "+var1+"-"+var2+" es= "+(var1-var2));
+        System.out.println("La multiplicacion de los numeros "+var1+"*"+var2+" es= "+(var1*var2));
+        System.out.println("La division de los numeros "+var1+"/"+var2+" es= "+division);
+
+        System.out.println("FIN");
+    }
+}
